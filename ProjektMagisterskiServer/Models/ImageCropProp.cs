a3069a28480babfc2537636c1790095133312b9c
Maciek Golowkin <maciek.golowkin@gmail.com>
2020-12-02 00:25:50 +0100

    Dodanie modelu

---
+++ ProjektMagisterskiServer/Models/ImageCropProp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektMagisterskiServer.Models
+{
+    public class ImageCropProp
+    {
+        public double x1;
+        public double x2;
+        public double y1;
+        public double y2;
+    }
+}
